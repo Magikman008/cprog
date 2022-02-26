45753aedebaa2026ca4309f28cf9f7d118aa0e3d
Dubov Andrew <Dubok008@yandex.ru>
2022-02-26 11:34:51 +0300

    first test

--- lab_01_02_01/main.c
+++ lab_01_02_01/main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+#include <math.h>
 
 int main()
 {
-    printf("Hello World!\n");
+    double a, b, f;
+    scanf("%lf%lf%lf", &a, &b, &f);
+    double alpha = f*3.14159265358979/180;
+    double s = fabs(pow(a, 2) - pow(b, 2)) * tan(alpha) / 2;
+    printf("%lf\n", s);
     return 0;
 }
