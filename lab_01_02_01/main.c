257f48dde4587443ec5f1902280451ef7e0d94e3
Dubov Andrew <Dubok008@yandex.ru>
2022-02-26 12:00:01 +0300

    update formula

--- lab_01_02_01/main.c
+++ lab_01_02_01/main.c
@@ -5,8 +5,9 @@ int main()
 {
     double a, b, f;
     scanf("%lf%lf%lf", &a, &b, &f);
-    double alpha = f*3.14159265358979/180;
-    double s = fabs(pow(a, 2) - pow(b, 2)) * tan(alpha) / 2;
+    double alpha = f * 3.14159265358979 / 180;
+    //double s = fabs(pow(a, 2) - pow(b, 2)) * tan(alpha) / 2;
+    double s = tan(alpha) * ((a - b) / 2) * ((a + b) / 2);
     printf("%lf\n", s);
     return 0;
 }
