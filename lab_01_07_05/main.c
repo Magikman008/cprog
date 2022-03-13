7b3a3cecd1b20b4071871d786aaa079eee70352b
Dubov Andrew <Dubok008@yandex.ru>
2022-03-13 22:52:30 +0300

    Fix 2 problem 7

--- lab_01_07_05/main.c
+++ lab_01_07_05/main.c
@@ -29,7 +29,7 @@ int main()
         printf("Wrong input\n");
         return ERROR_SCANF;
     }
-    if (e <= 0 || e > 1 || fabs(x) > 1)
+    if (e <= 0 || e > 1 || fabs(x) >= 1)
     {
         printf("Wrong input\n");
         return WRONG_E;
