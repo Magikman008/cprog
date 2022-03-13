6590c733e0dd5541d1f677cb254a54840dd7ef54
Dubov Andrew <Dubok008@yandex.ru>
2022-03-14 00:53:17 +0300

    Fix 1 problem 8

--- lab_01_08_01/main.c
+++ lab_01_08_01/main.c
@@ -19,20 +19,15 @@ int main()
 {
     unsigned x1, x2, x3, x4;
     int rc = scanf("%u%u%u%u", &x1, &x2, &x3, &x4);
-    if (rc != EXPECTED_SCANF_VALUE){
+    if (rc != EXPECTED_SCANF_VALUE)
+    {
         printf("Error: %d", ERROR_SCANF);
         return EXIT_FAILURE;
     }
-    /*if (x1 < 0 || x1 > 255 || x2 < 0 || x2 > 255 || x3 < 0 || x3 > 255 ||
-            x4 < 0 || x4 > 255)
-    {
-        printf("Error: %d", WRONG_INPUT);
-        return EXIT_FAILURE;
-    }*/
     printing(x1);
     printing(x2);
     printing(x3);
     printing(x4);
-    //printf("Result: %c", a);
+    printf(" %u %u %u %u", x1, x2, x3, x4);
     return EXIT_SUCCESS;
 }
