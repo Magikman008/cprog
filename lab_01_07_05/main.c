0786aaa8f990827304a16c90d4197475d63095db
Dubov Andrew <Dubok008@yandex.ru>
2022-03-13 22:33:29 +0300

    Problem 7

---
+++ lab_01_07_05/main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define EXPECTED_SCANF_VALUE 2
+#define ERROR_SCANF 1
+#define WRONG_E 1
+
+
+double sum(double x, double e)
+{
+    double cur = 1;
+    int degree = 2;
+    double sum = 0;
+    while (cur > e)
+    {
+        sum += cur;
+        cur = cur * x * x * (degree - 1) / degree;
+    }
+    return sum;
+}
+
+int main()
+{
+    double x, e;
+    int rc = scanf("%lf%lf", &x, &e);
+    if (rc != EXPECTED_SCANF_VALUE)
+    {
+        printf("Wrong input\n");
+        return ERROR_SCANF;
+    }
+    if (e <= 0 || e > 1 || x > 1)
+    {
+        printf("Wrong input\n");
+        return WRONG_E;
+    }
+
+    double f = 1 / sqrt(1 - x * x);
+    double s = sum(x, e);
+    double del = fabs(f - s);
+    double tet = fabs(f - s) / fabs(f);
+    printf("%lf %lf", del, tet);
+    return EXIT_SUCCESS;
+}
