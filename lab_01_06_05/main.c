ef0dccbb97a4c70c3a319390022fc4d1ca04e7aa
Dubov Andrew <Dubok008@yandex.ru>
2022-03-13 21:33:52 +0300

    Last fix problem 6

--- lab_01_06_05/main.c
+++ lab_01_06_05/main.c
@@ -4,6 +4,7 @@
 
 #define EXPECTED_SCANF_VALUE 8
 #define ERROR_SCANF 1
+#define WRONG_CORDS 1
 
 double vector(double vx1, double vy1, double vx2, double vy2)
 {
@@ -13,10 +14,10 @@ double vector(double vx1, double vy1, double vx2, double vy2)
 int checking(double x1, double y1, double x2, double y2, double x3, double y3,
 double x4, double y4)
 {
-    double v1 = vector(x4-x3,y4-y3,x1-x3,y1-y3);
-    double v2 = vector(x4-x3,y4-y3,x2-x3,y2-y3);
-    double v3 = vector(x2-x1,y2-y1,x3-x1,y3-y1);
-    double v4 = vector(x2-x1,y2-y1,x4-x1,y4-y1);
+    double v1 = vector(x4 - x3, y4 - y3, x1 - x3, y1 - y3);
+    double v2 = vector(x4 - x3, y4 - y3, x2 - x3, y2 - y3);
+    double v3 = vector(x2 - x1, y2 - y1, x3 - x1, y3 - y1);
+    double v4 = vector(x2 - x1, y2 - y1, x4 - x1, y4 - y1);
     if ((v1 * v2 < 0) && (v3 * v4 < 0))
     {
         return 1;
@@ -35,6 +36,11 @@ int main()
     {
         printf("Wrong input\n");
         return ERROR_SCANF;
+    }
+    if ((x1 == x2 && y1 == y2) || (x3 == x4 && y3 == y4))
+    {
+        printf("Wrong input\n");
+        return WRONG_CORDS;
     }
 
     printf("%d", checking(x1, y1, x2, y2, x3, y3, x4, y4));
