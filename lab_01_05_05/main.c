569a2457c75f1b31ebf6565a5eef35f4aa4874ad
Dubov Andrew <Dubok008@yandex.ru>
2022-03-13 17:09:40 +0300

    Fix wrong negative

--- lab_01_05_05/main.c
+++ lab_01_05_05/main.c
@@ -1,29 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define EXPECTED_SCANF_VALUE 1
+#define ERROR_SCANF 1
+#define ERROR_NUM 2
 
 void printing(int x)
 {
-    int tmp = 1;
-    while (tmp <= x)
+    if (x == 0)
     {
-        tmp *= 10;
-    }
-    tmp /= 10;
-    while (tmp > 0)
-    {
-        printf("%i", x / tmp);
-        x %= tmp;
+        printf("0\n");
+    } else {
+        int tmp = 1;
+        while (tmp <= x)
+        {
+            tmp *= 10;
+        }
         tmp /= 10;
+        while (tmp > 0)
+        {
+            printf("%i", x / tmp);
+            x %= tmp;
+            tmp /= 10;
+        }
+        printf("\n");
     }
-    printf("\n");
+
 }
 
 int main()
 {
     int n;
     int rc = scanf("%i", &n);
-    if (rc != 1 || n < 0)
-        return 0;
-    else
-        printing(n);
-    return 0;
+    if (rc != EXPECTED_SCANF_VALUE)
+    {
+        printf("Wrong input\n");
+        return ERROR_SCANF;
+    }
+    if (n < 0)
+    {
+        printf("Wrong input\n");
+        return ERROR_NUM;
+    }
+    printing(n);
+    return EXIT_SUCCESS;
 }
