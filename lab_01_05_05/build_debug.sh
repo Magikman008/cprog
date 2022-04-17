8cf3d51317160c937be4fedae033e8ca91b7b159
Dubov Andrew <Dubok008@yandex.ru>
2022-04-17 21:07:47 +0300

    Too late

--- lab_01_05_05/build_debug.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-gcc -std=c99 -Wall -Werror -Wpedantic -Wextra -Wfloat-equal -Wfloat-conversion -Wvla -O -g3 -c --coverage main.c -lm
-gcc -o app.exe --coverage main.o -lm
--- lab_01_05_05/build_release.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-gcc -std=c99 -Wall -Werror -Wextra -Wpedantic -Wfloat-equal -Wfloat-conversion -Wvla -O3 -c main.c -lm
-gcc -o app.exe main.o
