b3f856796226b2b5095c15551e1be5cf78164a1e
Dubov Andrew <Dubok008@yandex.ru>
2022-05-21 11:32:44 +0300

    first init 4

--- lab_04_04_01/main.c
+++ lab_04_04_01/main.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-#include <regex.h>
 
 #define MAX_LEN_OF_STR 257
 #define MAX_LEN_OF_WORD 17
@@ -10,13 +9,19 @@
 #define ERROR_EMPTY_STRING -1
 #define ERROR_TOO_BIG_WORD -2
 #define ERROR_TOO_LITTLE_WORDS -3
+#define ERROR_TOO_BIG_STR -4
 
 int input_str(char *string)
 {
-    if (!fgets(string, MAX_LEN_OF_STR, stdin))
+    if (!fgets(string, MAX_LEN_OF_STR + 1, stdin))
         return ERROR_EMPTY_STRING;
 
     size_t len = strlen(string);
+
+    if (string[len - 1] != '\n')
+    {
+        return ERROR_TOO_BIG_STR;
+    }
 
     if (string[len - 1] == '\n')
         string[--len] = '\0';
@@ -39,6 +44,73 @@ void change_string(char *string)
     }
 }
 
+int yes_or_no(char *string)
+{
+    // int len = strlen(string);
+    int cur = 0;
+
+    if (string[cur] == '-' || string[cur] == '+')
+        cur++;
+
+    int count = 0;
+
+    while (isdigit(string[cur]))
+    {
+        count++;
+        cur++;
+    }
+
+    if (count == 0)
+    {
+        if (string[cur] != '.')
+            return EXIT_FAILURE;
+
+        cur++;
+        count = 0;
+
+        while (isdigit(string[cur]))
+        {
+            count++;
+            cur++;
+        }
+
+        if (count == 0)
+            return EXIT_FAILURE;
+    }
+    else
+    {
+        if (string[cur] == '.')
+            cur++;
+
+        while (isdigit(string[cur]))
+        {
+            cur++;
+        }
+    }
+
+    if (string[cur] == 0)
+        return EXIT_SUCCESS;
+    if (string[cur] != 'E' && string[cur] != 'e')
+        return EXIT_FAILURE;
+    cur++;
+    if (string[cur] == '-' || string[cur] == '+')
+        cur++;
+
+    count = 0;
+
+    while (isdigit(string[cur]))
+    {
+        count++;
+        cur++;
+    }
+
+    if (count == 0)
+        return EXIT_FAILURE;
+    if (string[cur] != 0)
+        return EXIT_FAILURE;
+    return EXIT_SUCCESS;
+}
+
 int main(void)
 {
     char string[MAX_LEN_OF_STR];
@@ -52,13 +124,12 @@ int main(void)
         return ERROR_EMPTY_STRING;
 
     change_string(string);
-    printf("%s\n", string);
-    rc = regexec((regex_t *)string, "[+-]?(\\d+([.]\\d*)?([eE][+-]?\\d+)?|[.]\\d+([eE][+-]?\\d+)?)", 0, NULL, 0);
+    rc = yes_or_no(string);
 
-    if (rc == 0)
-        puts("Yes");
-    else
+    if (rc)
         puts("No");
+    else
+        puts("Yes");
 
     return EXIT_SUCCESS;
 }
