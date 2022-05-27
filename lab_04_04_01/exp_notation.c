304d86b84f02338b4291cedf6e8f9e5fe9b60162
Dubov Andrew <Dubok008@yandex.ru>
2022-05-28 01:40:08 +0300

    check symbols

--- lab_04_04_01/exp_notation.c
+++ lab_04_04_01/exp_notation.c
@@ -1,71 +1,99 @@
 #include "exp_notation.h"
 
+int isSigh(const char symbol)
+{
+    if (symbol == '-' || symbol == '+')
+        return EXIT_SUCCESS;
+
+    return EXIT_FAILURE;
+}
+
+int isEeee(const char symbol)
+{
+    if (symbol == 'E' || symbol == 'e')
+        return EXIT_SUCCESS;
+
+    return EXIT_FAILURE;
+}
+
+int isDot(const char symbol)
+{
+    if (symbol == '.')
+        return EXIT_SUCCESS;
+
+    return EXIT_FAILURE;
+}
+
+int isEnd(const char symbol)
+{
+    if (symbol == 0)
+        return EXIT_SUCCESS;
+
+    return EXIT_FAILURE;
+}
+
+int rowOfDigits(const char *string, int *shift, int *count)
+{
+    while (isdigit(string[*shift]))
+    {
+        (*count)++;
+        (*shift)++;
+    }
+
+    return EXIT_SUCCESS;
+}
+
 int isnumber(char *string)
 {
     int cur = 0;
 
-    if (string[cur] == '-' || string[cur] == '+')
+    if (!isSigh(string[cur]))
         cur++;
 
     int count = 0;
 
-    while (isdigit(string[cur]))
-    {
-        count++;
-        cur++;
-    }
+    rowOfDigits(string, &cur, &count);
 
     if (count == 0)
     {
-        if (string[cur] != '.')
+        if (isDot(string[cur]))
             return EXIT_FAILURE;
 
         cur++;
         count = 0;
 
-        while (isdigit(string[cur]))
-        {
-            count++;
-            cur++;
-        }
+        rowOfDigits(string, &cur, &count);
 
         if (count == 0)
             return EXIT_FAILURE;
     }
     else
     {
-        if (string[cur] == '.')
+        if (!isDot(string[cur]))
             cur++;
 
-        while (isdigit(string[cur]))
-        {
-            cur++;
-        }
+        rowOfDigits(string, &cur, &count);
     }
 
-    if (string[cur] == 0)
+    if (!isEnd(string[cur]))
         return EXIT_SUCCESS;
 
-    if (string[cur] != 'E' && string[cur] != 'e')
+    if (isEeee(string[cur]))
         return EXIT_FAILURE;
 
     cur++;
 
-    if (string[cur] == '-' || string[cur] == '+')
+    if (!isSigh(string[cur]))
         cur++;
 
     count = 0;
 
-    while (isdigit(string[cur]))
-    {
-        count++;
-        cur++;
-    }
+    rowOfDigits(string, &cur, &count);
 
     if (count == 0)
         return EXIT_FAILURE;
 
-    if (string[cur] != 0)
+    if (isEnd(string[cur]))
         return EXIT_FAILURE;
 
     return EXIT_SUCCESS;
