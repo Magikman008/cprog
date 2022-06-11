83815ecdb905768b11db3950bd2e739efba66e2a
Dubov Andrew <Dubok008@yandex.ru>
2022-06-11 09:37:16 +0300

    rework adding

--- lab_05_04_02/add_to_file.c
+++ lab_05_04_02/add_to_file.c
@@ -2,19 +2,16 @@
 
 void insert_to_array(good_t *goods, size_t count, good_t good)
 {
-    printf("%ld\n", count);
     size_t cur = count - 1;
-    printf("%" PRIu32 "\n", goods[cur].amount);
+    
     while (goods[cur].amount < good.amount)
     {
-        puts("1ok");
         goods[cur + 1] = goods[cur];
         cur--;
     }
 
     while (goods[cur].number < good.number && goods[cur].amount == good.amount)
     {
-        puts("2ok");
         goods[cur + 1] = goods[cur];
         cur--;
     }
