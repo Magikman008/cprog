0bc93a783b70f3fc5325d36fe49bc6eb4620e2ac
Dubov Andrew <Dubok008@yandex.ru>
2022-06-15 16:54:24 +0300

    www

--- lab_05_04_02/main.c
+++ lab_05_04_02/main.c
@@ -2,28 +2,40 @@
 
 int main(int argc, char **argv)
 {
+    if ((strcmp(argv[1], "st") != 0 && strcmp(argv[1], "ft") != 0 && strcmp(argv[1], "at") != 0))
+        return ERROR_WRONG_ARGS;
+
+    if (strcmp(argv[1], "st") == 0 && argc != 4)
+        return ERROR_WRONG_ARGS;
+
+    if (strcmp(argv[1], "ft") == 0 && argc != 4)
+        return ERROR_WRONG_ARGS;
+
+    if (strcmp(argv[1], "at") == 0 && argc != 3)
+        return ERROR_WRONG_ARGS;
+
     int rc;
 
-    if (argc == 4 && strcmp(argv[1], "ft") == 0)
+    if (strcmp(argv[1], "ft") == 0)
     {
         rc = case_ft(argv[2], argv[3]);
         if (rc)
             return rc;
     }
-    else if (argc == 4 && strcmp(argv[1], "st") == 0)
+
+    if (strcmp(argv[1], "st") == 0)
     {
         rc = case_st(argv[2], argv[3]);
         if (rc)
             return rc;
     }
-    else if (argc == 3 && strcmp(argv[1], "at") == 0)
+
+    if (strcmp(argv[1], "at") == 0)
     {
         rc = case_at(argv[2]);
         if (rc)
             return rc;
     }
-    else
-        return ERROR_WRONG_ARGS;
 
     return EXIT_SUCCESS;
 }
