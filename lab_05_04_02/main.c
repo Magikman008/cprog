3aae4837fc4c500014c0341ef1ea3c4a98e0b36d
Dubov Andrew <Dubok008@yandex.ru>
2022-06-15 15:31:18 +0300

    test sort

--- lab_05_04_02/main.c
+++ lab_05_04_02/main.c
@@ -4,18 +4,18 @@ int main(int argc, char **argv)
 {
     int rc;
 
-    if (argc == 4 && strcmp(argv[1], "ft") == 0)
+    /*if (argc == 4 && strcmp(argv[1], "ft") == 0)
     {
         rc = case_ft(argv[2], argv[3]);
         if (rc)
             return rc;
     }
-    // else if (argc == 4 && strcmp(argv[1], "st") == 0)
-    // {
-    //     rc = case_st(argv[2], argv[3]);
-    //     if (rc)
-    //         return rc;
-    // }
+    else */if (argc == 4 && strcmp(argv[1], "st") == 0)
+    {
+        rc = case_st(argv[2], argv[3]);
+        if (rc)
+            return rc;
+    }
     // else if (argc == 3 && strcmp(argv[1], "at") == 0)
     // {
     //     rc = case_at(argv[2]);
