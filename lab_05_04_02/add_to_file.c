234c3f1ccb21c0d16e856906719a7cd13e42eab5
Dubov Andrew <Dubok008@yandex.ru>
2022-06-15 12:15:03 +0300

    adding tests

--- lab_05_04_02/add_to_file.c
+++ lab_05_04_02/add_to_file.c
@@ -2,9 +2,9 @@
 
 void insert_to_array(good_t *goods, size_t count, good_t good)
 {
-    size_t cur = count - 1;
+    int cur = count - 1;
 
-    while (goods[cur].amount < good.amount || (goods[cur].number < good.number && goods[cur].amount == good.amount))
+    while (cur > -1 && (goods[cur].amount < good.amount || (goods[cur].number < good.number && goods[cur].amount == good.amount)))
     {
         goods[cur + 1] = goods[cur];
         cur--;
