260c595d719d87b4b18d0992a2ca3108687e0c87
Andrew Dubov <Dubok008@yandex.ru>
2022-11-11 21:57:02 +0300

    func tests

--- lab_07_01_05/collect_coverage.sh
+++ lab_07_01_05/collect_coverage.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-./clean.sh
-./build_debug.sh
-rm -rf main.gcda
+make debug
+
 ./func_tests/scripts/func_tests.sh
 echo
-gcov -rkH main.c add_to_file.c case.c print_file.c sort_file.c
+gcov -rkH out/*.o
