26c25a91b04b93d7c78aa5acbee77503947f2ef5
Andrew Dubov <Dubok008@yandex.ru>
2022-11-24 22:11:36 +0300

    styling

--- lab_07_01_05/src/main.c
+++ lab_07_01_05/src/main.c
@@ -3,8 +3,6 @@
 
 int main(int argc, char **argv)
 {
-    FILE *dist;
-
     if (argc != 3 && argc != 4)
         return ERROR_BAD_ARGS;
 
@@ -12,7 +10,6 @@ int main(int argc, char **argv)
         return ERROR_BAD_ARGS;
 
     size_t count = 0;
-
     int *nums = NULL;
 
     int rc = read_array(argv[1], &count, &nums);
@@ -20,24 +17,24 @@ int main(int argc, char **argv)
     if (rc)
         return rc;
 
+    FILE *dist;
+
     if (!(dist = fopen(argv[2], "w")))
         return ERROR_FILE_OPEN;
 
     int *pb_dst = nums, *pe_dst = nums + count;
+    free(nums);
 
     if (argc == 4)
     {
         rc = key(nums, nums + count, &pb_dst, &pe_dst);
-        free(nums);
 
         if (rc)
             return rc;
     }
 
     mysort(pb_dst, pe_dst - pb_dst, sizeof(int), compare_int);
-
     print_file(dist, pb_dst, pe_dst);
-
     free(pb_dst);
 
     if (fclose(dist))
